fb1ca9c3bb2e7af5024387e68c073b9effc7194d
Paul Guillard <paul@benuvillageesch.lu>
2022-11-03 14:14:32 +0100

    Updated pictures on the welcome page.

--- resources/views/includes/welcome/campaign.blade.php
+++ resources/views/includes/welcome/campaign.blade.php
@@ -5,7 +5,7 @@
 			<p class="welcome-campaign__left__title-2">{{ __('welcome.campaign-title-2') }}</p>
 			<div class="flex flex-col-reverse lg:flex-col">
 				<div class="welcome-campaign__left__img-container">
-					<img src="{{ asset('media/pictures/campaigns/DSC09261006.png') }}" class="m-auto">
+					<img src="{{ asset('media/pictures/welcome_picture_left.jpg') }}" class="m-auto">
 				</div>
 				<div class="flex justify-center flex-wrap welcome-campaign__left__links">
 
@@ -13,7 +13,7 @@
 			</div>
 		</div>
 		<div class="welcome-campaign__right">
-			<img src="{{ asset('media/pictures/campaigns/BC_WS_Photos_PoliticalDeclaration_8.jpg') }}" class="m-auto">
+			<img src="{{ asset('media/pictures/welcome_picture_right.jpg') }}" class="m-auto">
 		</div>
 	</div>
 </section>
