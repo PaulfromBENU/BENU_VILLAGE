4067ea5b8046d77a170866437d88a242c91c1356
Paul Guillard <paul@benuvillageesch.lu>
2022-11-15 16:52:13 +0100

    Improved back-office dashboard.

--- app/Filament/Widgets/MessagesOverview.php
+++ app/Filament/Widgets/MessagesOverview.php
@@ -23,12 +23,12 @@ class MessagesOverview extends BaseWidget
     protected function getCards(): array
     {
         return [
-            Card::make('Unanswered user messages', ContactMessage::where('is_answered', '0')->where('closed', '0')->count())
-            ->color('success'),
-            Card::make('Mask orders', MaskOrder::where('is_read', '0')->count())
-            ->color('success'),
-            Card::make("Small articles orders", ItemOrder::where('is_read', '0')->count())
-            ->color('success'),
+            // Card::make('Unanswered user messages', ContactMessage::where('is_answered', '0')->where('closed', '0')->count())
+            // ->color('success'),
+            // Card::make('Mask orders', MaskOrder::where('is_read', '0')->count())
+            // ->color('success'),
+            // Card::make("Small articles orders", ItemOrder::where('is_read', '0')->count())
+            // ->color('success'),
         ];
     }
 }
