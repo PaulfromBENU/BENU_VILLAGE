bade8f7b11ed06b758744b219b85709d5c98a24e
Paul Guillard <paul@benuvillageesch.lu>
2022-12-05 14:49:21 +0100

    Updated link in footer.

--- resources/views/footer/all_infos.blade.php
+++ resources/views/footer/all_infos.blade.php
@@ -6,7 +6,7 @@
 		{{ __('footer.all-txt-2-1') }} <strong>BENU COUTURE</strong>
 	</p>
 	<div class="flex justify-center flex-col md:flex-row w-full md:w-3/4 lg:w-1/2 m-auto">
-		<a href="{{ route('full-story-'.app()->getLocale()) }}" class="btn-couture btn-couture--transparent" style="min-width: fit-content;">{{ __('footer.all-story') }}</a>
+		<a href="{{ route('about-'.app()->getLocale()) }}" class="btn-couture btn-couture--transparent" style="min-width: fit-content;">{{ __('footer.all-story') }}</a>
 		<a href="{{ route('header.participate-'.app()->getLocale()) }}" class="btn-couture btn-couture--transparent">{{ __('footer.all-chart') }}</a>
 	</div>
 </section>
